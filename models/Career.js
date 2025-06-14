2a243aeb78b00b9579261acdbbdd1bfa5f08f48e
lokesh143b <srirangamlokeswararao@gmail.com>
2025-06-14 13:21:03 +0530

    added timestamps in Career.js

--- models/Career.js
+++ models/Career.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const careerSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  postedAt: { type: Date, default: Date.now },
-  expiresAt: { type: Date, required: true },
-});
+const careerSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    postedAt: { type: Date, default: Date.now },
+    expiresAt: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Career", careerSchema);
